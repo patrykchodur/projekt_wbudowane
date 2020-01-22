109984c133d8b592faac9cbd6e74686b23ea98aa
Patryk Chodur <patrykchodur@icloud.com>
2020-01-22 20:53:28 +0100

    player and recorder probably fixed still working on RAM though

--- sources/player_recorder.c
+++ sources/player_recorder.c
@@ -13,6 +13,7 @@ struct {
 	unsigned start;
 	int frequency;
 	char is_recording;
+	char unfinnished_record;
 } recorder_state;
 
 #ifndef PLAYER_TIMER_NO
@@ -34,6 +35,7 @@ void player_recorder_init(void) {
 	recorder_state.start = 0;
 	recorder_state.frequency = NO_SOUND;
 	recorder_state.is_recording = 0;
+	recorder_state.unfinnished_record = 0;
 
 	is_player_playing_val = 0;
 
@@ -54,36 +56,44 @@ void player_recorder_init(void) {
 
 
 	reset_next_sound_to_start();
+	// TODO dont erase
+	erase_saved();
+	reset_eeprom_position();
 }
 
 void start_record(int set_frequency) {
-	if (recorder_state.is_recording)
+	if (recorder_state.unfinnished_record)
 		end_record();
-	else
-		recorder_state.is_recording = 1;
+	recorder_state.is_recording = 1;
 	recorder_state.start = millis();
 	recorder_state.frequency = set_frequency;
+	recorder_state.unfinnished_record = 1;
 }
 
 // TODO implemet it
 static void append_to_eeprom(Sound to_save) {
 	current_position++;
 	*current_position = to_save;
-	current_position[1].frequency = -1;
+	current_position[1].frequency = NO_SOUND;
 
 }
 
 void end_record(void) {
+	if (!recorder_state.unfinnished_record)
+		return;
 	Sound to_save = {millis() - recorder_state.start,
 		recorder_state.frequency};
 	append_to_eeprom(to_save);
-	if (recorder_state.is_recording)
-		start_record(NO_SOUND);
+	recorder_state.unfinnished_record = 0;
+	start_record(SILENCE);
+
 }
 
 void end_recording(void) {
 	recorder_state.is_recording = 0;
 	end_record();
+	Sound end = {NO_SOUND, 0};
+	append_to_eeprom(end);
 }
 
 
@@ -110,13 +120,13 @@ void reset_next_sound_to_start(void) {
 }
 
 char has_next_sound() {
-	return current_position[1].frequency > 0;
+	return current_position[1].frequency != NO_SOUND;
 }
 
 // TODO implemet it
 void erase_saved(void) {
 
-	saved_sounds[0].frequency = -1; 
+	saved_sounds[0].frequency = NO_SOUND; 
 	reset_next_sound_to_start();
 }
 
@@ -145,15 +155,15 @@ static void start_next_sound(void) {
 	// setting match register - numer of tick
 	// to run handler
 	PLAYER_TIMER_LPC->MR0 = to_play.length_in_millis;
+	PLAYER_TIMER_LPC->TC = 0;
 
 	// turn on counter
 	PLAYER_TIMER_LPC->TCR = 1 << 0;
 
 	if (to_play.frequency > 0) {
 		start_sound(to_play.frequency);
-		is_player_playing_val = 1;
 	}
-		
+	is_player_playing_val = 1;
 }
 
 void PLAYER_TIMER_Handler(void) {
